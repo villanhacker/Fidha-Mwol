28de806e792cb5d1bfec43d183d098ec070798df
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-10-29 11:49:03 +0530

    Update textmaker.js

--- plugins/textmaker.js
+++ plugins/textmaker.js
@@ -6,7 +6,7 @@ This code works with unlimited and completely free an API scraper.
 Don't use it for illegal purposes.
 */
 
-const w5botapi = require('w5bot-textmaker'); // Import NPM Package
+const w5botapi = require('w5-textmaker'); // Import NPM Package
 
 const Asena = require('../events');
 const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
