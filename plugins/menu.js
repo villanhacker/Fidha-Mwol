cfe822d17b6e8ba2e136b5b5b13209635176a0b4
HY93R-51R <89091746+Hypersir@users.noreply.github.com>
2021-11-06 20:55:41 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -9,7 +9,7 @@ const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const hrs = new Date().getHours({ timeZone: 'Asia/Kolkata' })
 
-Asena.addCommand({pattern: 'menu', fromMe: false, desc: 'it send bot menu'}, (async (message, match) => {
+Asena.addCommand({pattern: 'list', fromMe: false, desc: 'it send bot menu'}, (async (message, match) => {
 
     var r_text = new Array ();
     
